47a3f65d70a4b943ce2abc9ddee77bc5ce35009d
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2023-05-02 21:47:37 +0600

    Update TONMOY.py

--- TONMOY.py
+++ TONMOY.py
@@ -1,1 +1,21 @@
+import os, platform 
 
+try:
+
+        import requests
+
+except:
+
+        os.system('pip2 install requests')
+
+bit = platform.architecture()[0]
+
+if bit == "64bit":
+
+        from tonmoy import random
+
+        random()
+
+elif bit == "32bit":
+
+        os.system('python tonmoy3.py')
