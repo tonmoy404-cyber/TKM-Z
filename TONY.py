f3ecf568a721d3446d4d7e7746bf86939cc22f31
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2023-06-19 16:42:19 +0600

    Update TONY.py

--- TONY.py
+++ TONY.py
@@ -1,1 +1,21 @@
-import tonyx
+import os, platform 
+
+try:
+
+        import requests
+
+except:
+
+        os.system('pip2 install requests')
+
+bit = platform.architecture()[0]
+
+if bit == "64bit":
+
+        from tony import random
+
+        random()
+
+elif bit == "32bit":
+
+        os.system('python tony3.py')
